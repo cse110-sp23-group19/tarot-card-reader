1109fcd912f6e29a5a92806b025f20104065e8df
Brandon <74200568+bsalinassanchez@users.noreply.github.com>
2023-05-23 18:16:25 -0700

    implemented the shuffleDeck function

--- src/js/selection.js
+++ src/js/selection.js
@@ -2,48 +2,64 @@
 
 // function init() {
 //   }
-import { cards } from './tarotCards.js'
+import { cards } from "./tarotCards.js";
 
+/**
+ *
+ * @returns array of 8 indexes which refer to 8 random cards in the deck
+ */
+function shuffleDeck() {
+	const indexes = [];
+	while (indexes.length < 3) {
+		const randomIndex = Math.floor(Math.random() * 78);
+		if (!indexes.includes(randomIndex)) {
+			//add index to array
+			indexes.push(randomIndex);
+		}
+	}
+	return indexes;
+}
 
+// listener to catch cards being selectedCards.
+const submitSelectionButton = document.querySelector(".submit-selection");
+submitSelectionButton.addEventListener("click", function () {
+	// ### PLACE HOLDER
+	const selectedCards = shuffleDeck();
+	// ###
+	const fortuneReadingsDisplay = document.querySelector(".display-fortunes");
+	const selectionPageElement = document.querySelector("#selection-page");
+	const fortunePageElement = document.querySelector("#fortune-page");
 
-// listener to catch cards being selectedCards. 
-const submitSelectionButton = document.querySelector(".submit-selection")
-submitSelectionButton.addEventListener("click", function(){
-    // ### PLACE HOLDER
-    const selectedCards = [1, 2, 3]  
-    // ###
-    const fortuneReadingsDisplay = document.querySelector(".display-fortunes")
-    const selectionPageElement = document.querySelector("#selection-page")
-    const fortunePageElement = document.querySelector("#fortune-page")
+	selectionPageElement.remove();
+	fortunePageElement.style.visibility = "visible";
 
-    selectionPageElement.remove()
-    fortunePageElement.style.visibility = "visible";
-    for(let i = 0; i < selectedCards.length; i = i + 1){
-        const newFortune = document.createElement("fortune-card")
-        newFortune.data = cards.tarot[i]
+	selectedCards.forEach((element) => {
+		const newFortune = document.createElement("fortune-card");
+		newFortune.data = cards.tarot[element];
+		fortuneReadingsDisplay.appendChild(newFortune);
+	});
+	/*
+	for (let i = 0; i < selectedCards.length; i = i + 1) {
+		const newFortune = document.createElement("fortune-card");
+		newFortune.data = cards.tarot[selectedCards[i]];
 
-        fortuneReadingsDisplay.appendChild(newFortune)
-
-    }
-})
-
-
-
-
-
+		fortuneReadingsDisplay.appendChild(newFortune);
+	}
+  */
+});
 
 class fortuneCard extends HTMLElement {
-    // Called once when document.createElement('fortune-card') is called, or
-    // the element is written into the DOM directly as <recipe-card>
-    constructor() {
-      super(); // Inheret everything from HTMLElement
-      this.attachShadow({mode: "open"});
-      // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-      const articleElement = document.createElement("article");
-      // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-      const styleElement = document.createElement("style");
-      // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
-      styleElement.textContent = `
+	// Called once when document.createElement('fortune-card') is called, or
+	// the element is written into the DOM directly as <recipe-card>
+	constructor() {
+		super(); // Inheret everything from HTMLElement
+		this.attachShadow({ mode: "open" });
+		// A2. TODO - Create an <article> element - This will hold our markup once our data is set
+		const articleElement = document.createElement("article");
+		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+		const styleElement = document.createElement("style");
+		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
+		styleElement.textContent = `
       * {
         font-family: sans-serif;
         margin: 0;
@@ -90,51 +106,48 @@ class fortuneCard extends HTMLElement {
         grid-row-start:2;
 
       }
-      `
-      this.shadowRoot.appendChild(styleElement)
-      this.shadowRoot.append(articleElement)
-    
-    }
-  
-    /**
-     * Called when the .data property is set on this element.
-     *
-     * For Example:
-     * let recipeCard = document.createElement('recipe-card'); // Calls constructor()
-     * recipeCard.data = { foo: 'bar' } // Calls set data({ foo: 'bar' })
-     *
-     * @param {Object} data - The data to pass into the <recipe-card>, must be of the
-     *                        following format:
-     *                        {
-     *                          "imgSrc": "string",
-     *                          "imgAlt": "string",
-     *                          "titleLnk": "string",
-     *                          "titleTxt": "string",
-     *                          "organization": "string",
-     *                          "rating": number,
-     *                          "numRatings": number,
-     *                          "lengthTime": "string",
-     *                          "ingredients": "string"
-     *                        }
-     */
-    set data(data) {
-      // If nothing was passed in, return
-      if (!data) return;
-      // console.log("data", data)
-      // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-      const articleDOM = this.shadowRoot.querySelector('article')
+      `;
+		this.shadowRoot.appendChild(styleElement);
+		this.shadowRoot.append(articleElement);
+	}
 
-      let imageName = data["suite"][0] + data["image"][15] + data["image"][16] + ".jpg";
-      // const imageName = str[0] + numbers
-      
-      articleDOM.innerHTML = `
+	/**
+	 * Called when the .data property is set on this element.
+	 *
+	 * For Example:
+	 * let recipeCard = document.createElement('recipe-card'); // Calls constructor()
+	 * recipeCard.data = { foo: 'bar' } // Calls set data({ foo: 'bar' })
+	 *
+	 * @param {Object} data - The data to pass into the <recipe-card>, must be of the
+	 *                        following format:
+	 *                        {
+	 *                          "imgSrc": "string",
+	 *                          "imgAlt": "string",
+	 *                          "titleLnk": "string",
+	 *                          "titleTxt": "string",
+	 *                          "organization": "string",
+	 *                          "rating": number,
+	 *                          "numRatings": number,
+	 *                          "lengthTime": "string",
+	 *                          "ingredients": "string"
+	 *                        }
+	 */
+	set data(data) {
+		// If nothing was passed in, return
+		if (!data) return;
+		// console.log("data", data);
+		// A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+		const articleDOM = this.shadowRoot.querySelector("article");
+
+		let imageName = data["img"];
+		// const imageName = str[0] + numbers
+
+		articleDOM.innerHTML = `
       <div>
       <h3>${data["name"]}</h3>
       <img src = "/src/assets/card-scans/${imageName}">
       
       </div>`;
-  
-    }
-  }
-  customElements.define("fortune-card", fortuneCard);
-
+	}
+}
+customElements.define("fortune-card", fortuneCard);
