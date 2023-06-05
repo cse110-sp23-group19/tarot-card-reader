93586875b29c5036bf8a9c7156142ca8e72da1f3
arl009 <97709149+arl009@users.noreply.github.com>
2023-06-05 02:07:41 -0700

    esLint

--- unit-tests/selection.test.js
+++ unit-tests/selection.test.js
@@ -1,10 +1,10 @@
-const functions = require('../src/js/selection.js');
+const functions = require("../src/js/selection.js");
 
-test('adds 1 + 2 to equal 3', () => {
+test("adds 1 + 2 to equal 3", () => {
     expect(1 + 2).toBe(3);
 });
 
-test('shuffling the deck only selects 12 cards, 100 times', () => {
+test("shuffling the deck only selects 12 cards, 100 times", () => {
     for(let i = 0; i < 100; i++){
         expect(functions.shuffleDeck().length).toBe(12);
     }
