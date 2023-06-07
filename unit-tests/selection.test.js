6013cfb167ed65554c194a294cf3a95b4fcdbc2a
arl009 <97709149+arl009@users.noreply.github.com>
2023-06-07 12:05:31 -0700

    3 unit tests

--- unit-tests/selection.test.js
+++ unit-tests/selection.test.js
@@ -1,11 +1,30 @@
 const functions = require("../src/js/selection.js");
 
+/*
 test("adds 1 + 2 to equal 3", () => {
     expect(1 + 2).toBe(3);
 });
+*/
 
-test("shuffling the deck only selects 12 cards, 100 times", () => {
-    for(let i = 0; i < 100; i++){
+test("shuffling the deck only selects 12 cards, 1000 times", () => {
+    for(let i = 0; i < 1000; i++){
         expect(functions.shuffleDeck().length).toBe(12);
+    }
+});
+
+test("shuffling the deck only selects cards with index less than 78, 1000 times", () => {
+    for(let i = 0; i < 1000; i++){
+        let deck = functions.shuffleDeck();
+        for(let j = 0; j < deck.length; j++){
+            expect(deck[j]).toBeLessThan(78);
+        }
+    }
+});
+
+test("all cards in deck are unique, 1000 times", () => {
+    for(let i = 0; i < 1000; i++){
+        let deck = functions.shuffleDeck();
+        const allUnique = deck => deck.length === new Set(deck).size;
+        expect(allUnique(deck)).toBe(true);
     }
 });
