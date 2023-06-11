b017b913ad8229b58779cd5fa1bc649f9c640e02
Ryan Goh <rygoh@ucsd.edu>
2023-06-10 23:46:11 -0700

    Clean up, documentation, hide shuffle button

--- src/js/selection.js
+++ src/js/selection.js
@@ -7,8 +7,10 @@
 	.then(json => cards = json);
 
 
-// This conditional checks if the code is running in a Node.js environment (common for testing)
-// If it is, it exports the 'shuffleDeck' function for use in testing
+/**
+ * This conditional checks if the code is running in a Node.js environment (common for testing)
+ * If it is, it exports the 'shuffleDeck' function for use in testing
+ */
 if (typeof module === "object") {
 	module.exports = { shuffleDeck };
 }
@@ -28,23 +30,21 @@ let selectedCards = [];
 const layout = document.getElementsByClassName("cards-to-select")[0];
 
 /**
- * brings the user back to the shuffle cards page to give them a new reading if they choose to
- * fortune-page
- * selection-page
+ * Brings the user back to the shuffle cards page to give them a new reading if they choose to
  */
 document.getElementById("get-new-reading").addEventListener("click", () => {
 	// remove fortune cards from fortune-page
 	let fortunes = document.getElementsByClassName("display-fortunes")[0];
 	fortunes.innerHTML = "";
-	//bring user back to card selection page
+	// bring user back to card selection page
 	document.getElementById("fortune-page").classList = "hidden";
 	document.getElementById("selection-page").classList = "visible";
-	//reset the selection page states
+	// reset the selection page states
 	resetSelectionPageStates();
 });
 
 /**
- * This function resets the states of the selection page. This means no cadrs have been
+ * Resets the states of the selection page. This means no cards have been
  * selected and the user is able to shuffle the deck
  */
 function resetSelectionPageStates() {
@@ -53,29 +53,36 @@ function resetSelectionPageStates() {
 	shuffleDeck();
 }
 
-
-// This button, when clicked, will reset the fortunes page, removing any current fortunes,
-// and return the user to the card selection page
-// It also resets the state of the card selection page by calling the 'resetSelectionPageStates' function
+/**
+ * This button resets the fortunes page, removing any current fortunes
+ * and return the user to the card selection page 
+ * It also resets the card selection page by calling the 'resetSelectionPageStates' function
+ */
 const removeButton = document.getElementById("remove");
 if (removeButton) {
 	removeButton.onclick = function () {
+		// bring user to the selection page, remove the landing page
 		document.getElementById("landing-page").remove();
 		document.getElementById("selection-page").classList = "visible";
-		shuffleDeck();
+		// initialize the selection page
+		resetSelectionPageStates();
 	};
 }
 
 /**
  * Shuffles the deck and displays 12 cards for selection
- * The shuffling operation only proceeds if no cards have been selected (selectedCards.length is 0)
+ * The shuffling operation only proceeds if no cards have been selected 
  */
 function shuffleDeck() {
+	// cannot shuffle if user has already selected a card
 	if (selectedCards.length != 0) {
 		return;
 	}
 	selectedCards = [];
 	indexes = [];
+	// displays the shuffle button on reset
+	document.querySelector(".shuffle-layout").style.visibility= "visible";
+	// randomize the cards
 	while (indexes.length < 12) {
 		const randomIndex = Math.floor(Math.random() * 78);
 		if (!indexes.includes(randomIndex)) {
@@ -88,15 +95,17 @@ function shuffleDeck() {
 }
 
 /**
- * function creates and displays each of the 12 shuffled cards to the display
+ * Creates and displays each of the 12 shuffled cards to the display
  */
 function displayShuffledCards() {
+	// for each of the 12 cards set the back card image, index, and add shuffle animation
 	indexes.forEach((index) => {
 		const newBackfaceCard = document.createElement("backface-card");
 		newBackfaceCard.index = index;
-		newBackfaceCard.classList.add("shuffle"); // Add shuffle animation
+		newBackfaceCard.classList.add("shuffle"); 
+		// remove the class after the animation
 		setTimeout(() => {
-			newBackfaceCard.classList.remove("shuffle"); // Remove the class after the animation
+			newBackfaceCard.classList.remove("shuffle"); 
 		}, 500);
 
 		if (layout) layout.appendChild(newBackfaceCard);
@@ -105,23 +114,25 @@ function displayShuffledCards() {
 }
 
 /**
- * sets up an event listener for each fortuneCard
+ * Sets up an event listener for each fortuneCard
  * If the card was "clicked", then it gets added to our array of selected cards
- * Before we can add, we must check if it was already selected. Or, if 3 cards
- * have already been selected, then the card will not be added. Prompts are shown
- * on screen that indicate these different events
- * @param {HTMLElement} card
+ * @param {HTMLElement} card card to add event listener to
  */
 let selectSound = document.getElementById("click-select");
 function addCardClickListener(card) {
 	card.addEventListener("click", function () {
+		// warn the user if card has already been selected
 		if (selectedCards.includes(card.index)) {
 			alert("This card has already been selected");
 			return;
 		}
+		// user selects new card, has not chosen 3 cards yet 
 		if (selectedCards.length < 3) {
 			selectedCards.push(card.index);
 			selectSound.play();
+			// hide shuffle button
+			document.querySelector(".shuffle-layout").style.visibility= "hidden";
+		// user has already chosen 3 cards, warn user
 		} else {
 			alert("you have selected 3 cards already");
 			backfaceCard.goldGlowCount = 3;
@@ -130,8 +141,8 @@ function addCardClickListener(card) {
 }
 
 /**
- * sets up an event listener for the shuffle button, shuffles the dislay on click.
- * Also, once the user clicks the shuffle button, a card shuffling sound will play
+ * Sets up an event listener for the shuffle button, shuffles the dislay on click
+ * Once the user clicks the shuffle button, a card shuffling sound will play
  */
 let hoverSounds = [
 	document.getElementById("hover-sound-1"),
@@ -142,9 +153,10 @@ let hoverSounds = [
 	document.getElementById("hover-sound-6"),
 ];
 
-
-// The 'shuffleDeck' function shuffles the deck and displays 12 cards for selection
-// The shuffling operation only proceeds if no cards have been selected (selectedCards.length is 0)
+/**
+ * Sets up the shuffle button functionality
+ * The button is only available if the user has not selected any cards.
+ */
 const shuffleButton = document.querySelector(".shuffle-layout");
 if (shuffleButton) {
 	shuffleButton.addEventListener("click", () => {
@@ -159,37 +171,38 @@ if (shuffleButton) {
 }
 
 /**
- * sets up an event listener for the submit button, displays the 3 selected cards
+ * Sets up an event listener for the submit button, displays the 3 selected cards
  * The 'submitButton' element, when clicked, checks if 3 cards have been selected
- * If less than 3 cards have been selected, it gives an alert message
- * If exactly 3 cards have been selected, it plays a sound and displays the fortunes page
  */
 let submitSound = document.getElementById("submit-sound");
 const submitButton = document.querySelector(".submit-selection");
 if (submitButton) {
 	submitButton.addEventListener("click", function () {
+		// if less than 3 cards have been selected, give an alert message
 		if (selectedCards.length < 3) {
 			alert("you do not have 3 cards selected");
 			return;
 		}
+		// 3 cards have been selected, play a sound and display the fortunes page
 		submitSound.play();
 		displayFortunePage();
 	});
 }
 
 /**
- * removes the selection page, and makes the selected cards visible to user
+ * Removes the selection page, makes the selected cards visible to user
  * and displays the selected cards on the fortunes page
  */
 function displayFortunePage() {
 	const fortuneReadingsDisplay = document.querySelector(".display-fortunes");
 	const selectionPageElement = document.querySelector("#selection-page");
 	const fortunePageElement = document.querySelector("#fortune-page");
-
+	// switch from selection page to display fortune page
 	selectionPageElement.classList = "hidden";
 	fortunePageElement.style.visibility = "visible";
 	fortunePageElement.classList = "visible";
-
+	// for each of the selected cards, set the card info to 
+	// the card details in tarotCard.js
 	selectedCards.forEach((cardIndex) => {
 		const newCard = document.createElement("fortune-card");
 		newCard.data = cards.tarot[cardIndex];
@@ -197,20 +210,28 @@ function displayFortunePage() {
 	});
 }
 /**
- * A class to represent the back of the cards
+ * Back of fortune card custom HTML element for selection page
  */
 class backfaceCard extends HTMLElement {
 	static goldGlowCount = 0;
 	static maxGoldGlow = 3;
-
+	/**
+	 * Constructor for class backfaceCard
+	 */
 	constructor() {
 		super();
 		this._index = null;
 		this._glowing = false;
 		this.attachShadow({ mode: "open" });
 	}
+	/**
+	 * connectedCallback() is called anytime
+	 * the backfaceCard element is added to the
+	 * document
+	 */
 	connectedCallback() {
-		this.shadowRoot.innerHTML = `
+		// set the css styling of the fortunes to select
+		this.shadowRoot.innerHTML = ` 
 		<style>
 			div {
 				width: 200px;
@@ -231,28 +252,37 @@ class backfaceCard extends HTMLElement {
 			<img src = "./assets/card-scans/backface-card.jpg">
 		</div>`;
 
-		// Add event listener for the click
+		// add event listener if a card is selected
 		this.shadowRoot.querySelector("#card").addEventListener("click", () => {
 			if (!this._glowing && backfaceCard.goldGlowCount < backfaceCard.maxGoldGlow) {
 				this.shadowRoot.querySelector("#card").classList.add("goldGlow");
-				this._glowing = true; // Update instance variable
-				backfaceCard.goldGlowCount++; // Increase count
+				this._glowing = true; 
+				backfaceCard.goldGlowCount++; 
 			}
 		});
 	}
+	/**
+	 * returns the index of the backfaceCard
+	 */
 	get index() {
 		return this._index;
 	}
-
+	/**
+	 * sets the index of the backfaceCard
+	 * @param {Number} index number to set index to
+	 */
 	set index(index) {
 		this._index = index;
 	}
 }
 
 /**
- * Fortune card custom HTML element
+ * Fortune card custom HTML element for display page
  */
 class fortuneCard extends HTMLElement {
+	/**
+	 * Constructor for class fortuneCard
+	 */
 	constructor() {
 		super();
 		this.attachShadow({ mode: "open" });
@@ -331,45 +361,26 @@ class fortuneCard extends HTMLElement {
 		this.shadowRoot.append(articleElement);
 	}
 
-	/**
-	 * @param {Object} data - The data to pass into the <recipe-card>, must be of the
-	 *                        following format:
-	 *                        {
-	 *                          "imgSrc": "string",
-	 *                          "imgAlt": "string",
-	 *                          "titleLnk": "string",
-	 *                          "titleTxt": "string",
-	 *                          "organization": "string",
-	 *                          "rating": number,
-	 *                          "numRatings": number,
-	 *                          "lengthTime": "string",
-	 *                          "ingredients": "string"
-	 *                        }
+	/** 
+	 * Matches the selected cards to the card image, name, and meaning
+	 * @param {Object} data index from the "tarot" array in the tarotCards.json file
 	 */
 	set data(data) {
-		// If nothing was passed in, return
+		// if nothing was passed in, return
 		if (!data) return;
 		const articleDOM = this.shadowRoot.querySelector("article");
-
 		let imageName = data["img"];
-
 		articleDOM.innerHTML = `
-      <div class="fortune-card-container">
-
-      <h3 class="card-title">${data["name"]}</h3>
-	  <br><br>
-      <img src = "./assets/card-scans/${imageName}">
-	  <p> Your interpretation: </p>
-	  <p>${data["interpretation"]} </p>
-      </div>`;
+    	<div class="fortune-card-container">
+		<h3 class="card-title">${data["name"]}</h3>
+		<br><br>
+		<img src = "./assets/card-scans/${imageName}">
+		<p> Your interpretation: </p>
+		<p>${data["interpretation"]} </p>
+		</div>`;
 	}
 }
 
 customElements.define("backface-card", backfaceCard);
 customElements.define("fortune-card", fortuneCard);
 
-
-/**
- * Calls shuffleDeck on window load in order to setup the initial layout
- */
-//window.onload = shuffleDeck();
--- src/js/site-sounds.js
+++ src/js/site-sounds.js
@@ -1,12 +1,14 @@
-// Retrieve the HTML element for the background music
+// retrieve the HTML element for the background music
 let music = document.getElementById("bg-music");
-// Retrieve the HTML element for the mute button
+// retrieve the HTML element for the mute button
 const playButton = document.getElementById("mute-button");
 
-// Function to get the music muted state from localStorage
+/**
+ * Function to get the music muted state from localStorage
+ */
 function getMusicMutedState() {
 	try {
-		// Return the music muted state from localStorage
+		// return the music muted state from localStorage
 		return localStorage.getItem("musicMuted");
 	} catch (error) {
 		// localStorage is not available, return a default value
@@ -14,36 +16,35 @@ function getMusicMutedState() {
 	}
 }
 
-// Check localStorage to see if the music was playing previously
-// Check localStorage to see if the music was muted previously
-// If it was, then mute the music on page load
+// check if music has been muted
 if (getMusicMutedState() === "true") {
 	music.muted = true;
 }
 
-// Function to toggle the state of the music (either mute or unmute)
+/**
+ * Function to toggle the state of the music (either mute or unmute)
+ */
 function toggleMusic() {
 	if (music.muted) {
-		// If music is muted, unmute and reset the time from local storage
+		// if music is muted, unmute and reset the time from local storage
 		music.muted = false;
-		//set time from local storage
 		music.currentTime = parseInt(localStorage.getItem("time"));
-		// Save the unmuted state to localStorage
 		setMusicMutedState("false");
 	} else {
-		// If music is not muted, mute it and save the current time to local storage
+		// if music is not muted, mute it and save the current time to local storage
 		music.muted = true;
-		//set time in local storage
 		localStorage.setItem("time", music.currentTime.toString());
-		// Save the muted state to localStorage
 		setMusicMutedState("true");
 	}
 }
 
-// Function to set the music muted state in localStorage
+/**
+ * Function to set the music muted state in localStorage
+ * @param {*} value 
+ */
 function setMusicMutedState(value) {
 	try {
-		// Set the music muted state in localStorage
+		// set the music muted state in localStorage
 		localStorage.setItem("musicMuted", value);
 	} catch (error) {
 		// localStorage is not available, can't store the value
@@ -51,16 +52,14 @@ function setMusicMutedState(value) {
 	}
 }
 
-// Add an event listener to the mute button that will toggle the music state when clicked
-
+// add an event listener to the mute button that will toggle the music state when clicked
 playButton.addEventListener("click", () => {
 	toggleMusic();
 });
 
-// Set the initial state of the music when the window loads
-
+// set the initial state of the music when the window loads
 window.onload = () => {
 	music = document.getElementById("bg-music");
-	//set music time to what is in local storate
+	// set music time to what is in local storate
 	music.currentTime = parseInt(localStorage.getItem("time"));
 };
